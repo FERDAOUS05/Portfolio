c7caba9b384af31de8a1e1e710b85b45f8d535a3
meriem <meriemboukir26@gmail.com>
2024-06-18 15:01:13 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+/* SCROLL EDUCATION */
+srtop.reveal('.education .box', { interval: 200 });
